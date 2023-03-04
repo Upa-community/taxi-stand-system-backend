cc8c70720c363ada1e731eccebafe07012c7d38c
西海大愛 <71867595+nishiumidaina@users.noreply.github.com>
2023-03-05 00:50:58 +0900

    Laravelの環境構築

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterApiController;
+use App\Http\Controllers\Auth\LoginApiController;
+use App\Http\Controllers\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// 認証
+Route::post('/register', [RegisterApiController::class, 'register']);
+Route::post('/login', [LoginApiController::class, 'login']);
+Route::post('/logout', [LoginApiController::class, 'logout']);
+Route::middleware('auth:sanctum')->group(function(){
+});
