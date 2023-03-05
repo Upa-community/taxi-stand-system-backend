c4006ed7e15016067432f6e084c25cebec2e8346
shiratoriyuma <126394996+shiratoriyuma@users.noreply.github.com>
2023-03-05 23:17:18 +0900

    spotsテーブルのマイグレーションファイルを追加

---
+++ database/migrations/2023_03_05_140224_create_spots_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSpotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('spots', function (Blueprint $table) {
+            $table->id();
+            $table->string('spots_name');
+            $table->string('spots_address');
+            $table->string('spots_latitude');
+            $table->string('spots_longitude');
+            $table->string('spots_url');
+            $table->string('spots_status');
+            $table->integer('spots_count');
+            $table->string('spots_day_count');
+            $table->string('spots_month_count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('spots');
+    }
+}
