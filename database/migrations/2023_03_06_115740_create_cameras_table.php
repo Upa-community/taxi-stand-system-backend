2c779e3504df672b6e433a32def6010b72c30fba
shiratoriyuma <126394996+shiratoriyuma@users.noreply.github.com>
2023-03-06 21:05:38 +0900

    camerasテーブルのマイグレーションファイルを追加

---
+++ database/migrations/2023_03_06_115740_create_cameras_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCamerasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cameras', function (Blueprint $table) {
+            $table->id();
+            $table->integer('spots_id');
+            $table->string('cameras_name');
+            $table->string('cameras_url');
+            $table->string('cameras_status');
+            $table->integer('cameras_count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cameras');
+    }
+}
