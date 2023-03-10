4c9f0992b05afc190c00c23e95e8772dbd708d0a
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-03-11 08:16:41 +0900

    グラフ生成用にレスポンスの内容を修正

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -9,6 +9,9 @@ use App\Models\Spot\Spot;
 class HomeController extends Controller
 {
     public function homeData($usersId) {
+        $spotsData = [];
+        $monthLabelsData = [];
+        $dayLabelsData = [];
         $spots = Spot::where("users_id", $usersId)->get([
             "id",
             "spots_name",
@@ -22,6 +25,49 @@ class HomeController extends Controller
             "spots_month_count",
         ]);
 
-        return $spots;
+        for ($i = 0; $i < count($spots); $i++) {
+            $spotsDayCount = explode(",", $spots[$i]["spots_day_count"]);
+            $spotsMonthCount = explode(",", $spots[$i]["spots_month_count"]);
+            $r = rand(0, 240);
+            $g = rand(0, 240);
+            $b = rand(0, 240);
+            $borderColor = "rgba(". (string)$r . ", " . (string)$g . ", " . (string)$b . ", " . "0.6)";
+
+            $data = [
+                "id" => $spots[$i]["id"],
+                "spots_name" => $spots[$i]["spots_name"],
+                "spots_latitude" => $spots[$i]["spots_latitude"],
+                "spots_longitude" => $spots[$i]["spots_longitude"],
+                "spots_address" => $spots[$i]["spots_address"],
+                "spots_url" => $spots[$i]["spots_url"],
+                "spots_status" => $spots[$i]["spots_status"],
+                "spots_count" => $spots[$i]["spots_count"],
+                "spots_day_count" => $spotsDayCount,
+                "spots_month_count" => $spotsMonthCount,
+                "border_color" => $borderColor
+            ];
+            
+            array_push($spotsData, $data);
+        }
+
+        for ($i = 0; $i < 24; $i++) {
+            $objDateTime = date('Y-m-d', strtotime("-$i day"));
+            array_push($dayLabelsData, $i);
+        }
+
+        for ($i = 0; $i < 30; $i++) {
+            $objDateTime = date('Y-m-d', strtotime("-$i day"));
+            array_push($monthLabelsData, $objDateTime);
+        }
+
+        $monthLabelsData = array_reverse($monthLabelsData);
+
+        $response = [
+            "spots_data" => $spotsData,
+            "day_labels_data" => $dayLabelsData,
+            "month_labels_data" => $monthLabelsData
+        ];
+
+        return $response;
     }
 }
