55c9fd2e1dc1bf8b7e54011c07818d9c9c4a3c36
shiratoriyuma <126394996+shiratoriyuma@users.noreply.github.com>
2023-06-13 23:15:54 +0900

    メソッドの統一化

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -9,9 +9,6 @@ use App\Models\Spot\Spot;
 class HomeController extends Controller
 {
     public function homeData($usersId) {
-        $spotsData = [];
-        $monthLabelsData = [];
-        $dayLabelsData = range(0, 23);
         $spots = Spot::where("users_id", $usersId)->get([
             "id",
             "spots_name",
@@ -26,70 +23,23 @@ class HomeController extends Controller
             "spots_month_count",
         ]);
 
-        for ($i = 0; $i < count($spots); $i++) {
-            $spotsDayCount = explode(",", $spots[$i]["spots_day_count"]);
-            $spotsMonthCount = explode(",", $spots[$i]["spots_month_count"]);
-            $r = rand(130, 255);
-            $g = rand(130, 255);
-            $b = rand(180, 255);
-            $borderColor = "rgba(". (string)$r . ", " . (string)$g . ", " . (string)$b . ", " . "1)";
+        $spotsData = $this->createSpotsData($spots);
+        $weekData = $this->createWeekData();
+        $monthLabelsData = $this->createMonthLabelsData();
+        $daylabelsData = range(0, 23);
 
-            $data = [
-                "id" => $spots[$i]["id"],
-                "spots_name" => $spots[$i]["spots_name"],
-                "spots_latitude" => $spots[$i]["spots_latitude"],
-                "spots_longitude" => $spots[$i]["spots_longitude"],
-                "spots_address" => $spots[$i]["spots_address"],
-                "spots_url" => $spots[$i]["spots_url"],
-                "spots_status" => $spots[$i]["spots_status"],
-                "spots_max" => $spots[$i]["spots_max"],
-                "spots_count" => $spots[$i]["spots_count"],
-                "spots_day_count" => $spotsDayCount,
-                "spots_month_count" => $spotsMonthCount,
-                "border_color" => $borderColor
-            ];
-
-            array_push($spotsData, $data);
-        }
-
-        $week = ["日", "月", "火", "水", "木", "金", "土"];
-        $weekResult = [];
-        $weekData = [];
-
-        for ($i = 0; $i < 7; $i++) {
-            $objDateTime = strtotime("-$i day");
-            $dayOfWeek = date('w', $objDateTime);
-            array_push($weekResult, $week[$dayOfWeek]);
-        }
-
-        for ($i = 0; $i < 5; $i++) {
-            $weekData = array_merge($weekData, $weekResult);
-        }
-
-        $weekData = array_reverse($weekData);
-
-        for ($i = 0; $i < 35; $i++) {
-            $objDateTime = date('Y-m-d', strtotime("-$i day"));
-            array_push($monthLabelsData, $objDateTime);
-        }
-
-        $monthLabelsData = array_reverse($monthLabelsData);
-
-        $response = [
+        return [
             "spots_data" => $spotsData,
             "spots_week" => $weekData,
-            "day_labels_data" => $dayLabelsData,
+            "day_labels_data" => $daylabelsData,
             "month_labels_data" => $monthLabelsData
         ];
-
-        return $response;
     }
 
     public function homeSearch($usersId, $searchWord) {
-        $spotsData = [];
-        $monthLabelsData = [];
-        $dayLabelsData = range(0, 23);
-        $spots = Spot::where("users_id", $usersId)->where("spots_name", "LIKE", "%" . $searchWord . "%")->get([            "spots_name",
+        $spots = Spot::where("users_id", $usersId)->where("spots_name", "LIKE", "%" . $searchWord . "%")->get([
+            "id",
+            "spots_name",
             "spots_latitude",
             "spots_longitude",
             "spots_address",
@@ -101,6 +51,22 @@ class HomeController extends Controller
             "spots_month_count",
         ]);
 
+        $spotsData = $this->createSpotsData($spots);
+        $weekData = $this->createWeekData();
+        $monthLabelsData = $this->createMonthLabelsData();
+        $daylabelsData = range(0, 23);
+
+        return [
+            "spots_data" => $spotsData,
+            "spots_week" => $weekData,
+            "day_labels_data" => $daylabelsData,
+            "month_labels_data" => $monthLabelsData
+        ];
+    }
+
+    private function createSpotsData($spots) {
+        $spotsData = [];
+
         for ($i = 0; $i < count($spots); $i++) {
             $spotsDayCount = explode(",", $spots[$i]["spots_day_count"]);
             $spotsMonthCount = explode(",", $spots[$i]["spots_month_count"]);
@@ -127,6 +93,10 @@ class HomeController extends Controller
             array_push($spotsData, $data);
         }
 
+        return $spotsData;
+    }
+
+    private function createWeekData() {
         $week = ["日", "月", "火", "水", "木", "金", "土"];
         $weekResult = [];
         $weekData = [];
@@ -143,6 +113,12 @@ class HomeController extends Controller
 
         $weekData = array_reverse($weekData);
 
+        return $weekData;
+    }
+
+    private function createMonthLabelsData() {
+        $monthLabelsData = [];
+
         for ($i = 0; $i < 35; $i++) {
             $objDateTime = date('Y-m-d', strtotime("-$i day"));
             array_push($monthLabelsData, $objDateTime);
@@ -150,13 +126,6 @@ class HomeController extends Controller
 
         $monthLabelsData = array_reverse($monthLabelsData);
 
-        $response = [
-            "spots_data" => $spotsData,
-            "spots_week" => $weekData,
-            "day_labels_data" => $dayLabelsData,
-            "month_labels_data" => $monthLabelsData
-        ];
-
-        return $response;
+        return $monthLabelsData;
     }
 }
